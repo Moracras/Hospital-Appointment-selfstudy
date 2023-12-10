2905e94caf2e2640d300d001325c7abac0963cc8
Moracras <tavfikkarademir@gmail.com>
2023-12-10 03:44:35 +0300

    last edit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./pages/Home"
 function App() {
   return (
     <div>
-      <h1>Hospital Appointment System</h1>
       <Home/>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-
+import AppointmentList from '../components/AppointmentList'
 import Doctors from '../components/Doctors'
 
 const Home = () => {
   return (
+    <div>
     <Doctors/>
+    <AppointmentList/>
+    </div>
+    
   )
 }
 
